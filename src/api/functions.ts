3fffb712eae2078ba6e6bd0d10c5555bd1c68cb2
Jozef Bachorík <jozefbachorik@gmail.com>
2024-02-21 19:12:27 +0100

    - kompler v1.2

--- src/api/functions.ts
+++ src/api/functions.ts
@@ -1,5 +1,3 @@
-import Resizer from "react-image-file-resizer";
-
 export const findValueByKey = (array: any, key: any) => {
     if (array && key in array && array[key] !== "") {
         return array[key];
--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import { TitleProps } from '../../api/interfaces';
- 
+
 const Title: React.FC<TitleProps> = ({ title, specialClass = "", headingLevel = 1 }) => {
 
-    const safeHeadingLevel = Math.min(3, Math.max(1, headingLevel));
-  // Define a mapping of heading levels to class names
+  const safeHeadingLevel = Math.min(3, Math.max(1, headingLevel));
   const headingClassNames: Record<number, string> = {
     1: 'text-6xl mb-4',
     2: 'text-4xl mb-2',
     3: 'text-2xl mb-2',
   };
 
-  // Determine the class name based on the safeHeadingLevel
   const className = `${headingClassNames[safeHeadingLevel]} ${specialClass}`;
 
-  // Create the heading element with the determined class name
   const heading = React.createElement(`h${safeHeadingLevel}`, { className }, title);
 
-  
-    return <>{heading}</>;
-  };
-  
-  export default Title;
+
+  return <>{heading}</>;
+};
+
+export default Title;
